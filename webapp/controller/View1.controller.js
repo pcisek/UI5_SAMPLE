db7a6827b47b1d9cd40290a0d0783e8e007e351a
Piotr Cisek <p.cisek@corpnet.local>
2024-07-11 11:24:34 +0200

    v1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("project1.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
